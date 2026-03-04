ec610448100110031bdeaa604796126cf0237828
openjkai <29139931+openjkai@users.noreply.github.com>
2026-03-04 12:26:09 +0000

    feat: integrate @nestjs/config and create initial modules for authentication, organizations, and users

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { OrganizationsModule } from './modules/organizations/organizations.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+    OrganizationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
